f07cb03a367a229d870ec904806dca0c6b400a12
octavian <coctavian10@yahoo.ro>
2021-01-15 00:34:36 +0200

    Matching bug fix

--- strategies/heuristic.py
+++ strategies/heuristic.py
@@ -6,41 +6,45 @@ def moves(board):
     return np.argwhere(board == 0)
 
 
-def evaluate(board, patterns, c):
+def evaluate(board, patterns, cl):
     total_score = 0
     score = 0
 
     max_score = 0
     for i, row in enumerate(board):
         s = ''.join(map(str, row))
-        for match in re.findall(c, s):
-            score += patterns.get(match, patterns.get(match[::-1], 0))
-            max_score = max(score, max_score)
+        for c in cl:
+            for match in re.findall(c, s):
+                score += patterns.get(match, patterns.get(match[::-1], 0))
+                max_score = max(score, max_score)
     total_score += max_score
 
     max_score = 0
     for i, row in enumerate(board.T):
         s = ''.join(map(str, row))
-        for match in re.findall(c, s):
-            score += patterns.get(match, patterns.get(match[::-1], 0))
-            max_score = max(score, max_score)
+        for c in cl:
+            for match in re.findall(c, s):
+                score += patterns.get(match, patterns.get(match[::-1], 0))
+                max_score = max(score, max_score)
     total_score += max_score
 
     max_score = 0
     for i in range(-board.shape[0] + 1, board.shape[0] - 1):
         s = ''.join(map(str, np.diag(board, i)))
-        for match in re.findall(c, s):
-            score += patterns.get(match, patterns.get(match[::-1], 0))
-            max_score = max(score, max_score)
+        for c in cl:
+            for match in re.findall(c, s):
+                score += patterns.get(match, patterns.get(match[::-1], 0))
+                max_score = max(score, max_score)
     total_score += max_score
 
     max_score = 0
     flipped_board = np.flip(board, 1)
     for i in range(-flipped_board.shape[0] + 1, flipped_board.shape[0] - 1):
         s = ''.join(map(str, np.diag(flipped_board, i)))
-        for match in re.findall(c, s):
-            score += patterns.get(match, patterns.get(match[::-1], 0))
-            max_score = max(score, max_score)
+        for c in cl:
+            for match in re.findall(c, s):
+                score += patterns.get(match, patterns.get(match[::-1], 0))
+                max_score = max(score, max_score)
     total_score += max_score
 
     return score
@@ -52,21 +56,20 @@ last_score = 0
 def next_move(board):
     global last_score
     patterns = {'11111': 12 ** 2,
-                '011110': 11 ** 2,
-                '01111': 10 ** 2,
+                '011110': 9 ** 2,
+                '01111': 8 ** 2,
                 '01110': 4 ** 2,
-                '11100': 3 ** 2,
-                '01100': 1 ** 2,
+                '11100': 2 ** 2,
+                '01100': 1,
                 '212': 1,
-                '00111': 2 ** 2,
+                '221': 3,
                 '02221': 6 ** 2,
                 '22210': 5 ** 2,
-                '2212': 9 ** 2,
-                '22122': 9 ** 2,
-                '21222': 9 ** 2,
-                '22221': 9 ** 2}
-    c = re.compile(f"(?=({'|'.join(list(patterns.keys()) + [s[::-1] for s in patterns.keys()])}))")
-
+                '2212': 10 ** 2,
+                '22122': 10 ** 2,
+                '21222': 10 ** 2,
+                '22221': 10 ** 2}
+    c = list(map(re.compile, list(patterns.keys()) + ([s[::-1] for s in patterns.keys()])))
     best = (None, None)
     for move in moves(board.board):
         board.place(move, 1)
