3b4d5c55e2d7bfc1fef1d9857bea867ab687546e
octavian <coctavian10@yahoo.ro>
2021-01-14 14:20:05 +0200

    Render pieces
    Co-Authored-By: AdutP <AdutP@users.noreply.github.com>

--- views/game.py
+++ views/game.py
@@ -12,25 +12,51 @@ class Game(GomokuView):
         super().__init__(screen)
         self.board = board
 
+        size = self.board.size - 1
+
+        if self.board.size == 15:
+            self.space = 30
+        else:
+            self.space = Game.SPACE
+
+        self.vertical = (globals.WIDTH - size * self.space) // 2
+        assert (self.vertical >= Game.PADDING_VERTICAL)
+
+        self.horizontal = (globals.HEIGHT - size * self.space - Game.PADDING_TOP) // 2
+        assert (self.horizontal >= Game.PADDING_BOTTOM)
+
+        self.top = Game.PADDING_TOP
+
+        self.board.matrix[0][0] = 1
+        self.board.matrix[0][1] = 2
+
+        self.board.matrix[14][1] = 2
+
+    def render_board(self):
+        for x in range(self.board.size):
+            self.draw_line((self.vertical + x * self.space, self.top + self.horizontal),
+                           (self.vertical + x * self.space, globals.HEIGHT - self.horizontal),
+                           1, (0, 0, 0))
+
+            self.draw_line((self.vertical, self.top + self.horizontal + x * self.space),
+                           (globals.WIDTH - self.vertical, self.top + self.horizontal + x * self.space),
+                           1, (0, 0, 0))
+
+    def render_pieces(self):
+        for i in range(self.board.size):
+            for j in range(self.board.size):
+                if self.board.matrix[i][j] == 0:
+                    continue
+
+                player1 = self.board.matrix[i][j] == 1
+
+                self.draw_circle(self.space // 2 - 2,
+                                 (self.vertical + i * self.space,
+                                  self.top + self.horizontal + j * self.space),
+                                 (255, 255, 255) if player1 else (0, 0, 0))
+
     def render(self):
         super().render()
 
-        size = self.board.size - 1
-        space = Game.SPACE
-
-        vertical = (globals.WIDTH - size * space) // 2
-        assert (vertical >= Game.PADDING_VERTICAL)
-
-        horizontal = (globals.HEIGHT - size * space - Game.PADDING_TOP) // 2
-        assert (horizontal >= Game.PADDING_BOTTOM)
-
-        top = Game.PADDING_TOP
-
-        for x in range(self.board.size):
-            self.draw_line((vertical + x * space, top + horizontal),
-                           (vertical + x * space, globals.HEIGHT - horizontal),
-                           1, (0, 0, 0))
-
-            self.draw_line((vertical, top + horizontal + x * space),
-                           (globals.WIDTH - vertical, top + horizontal + x * space),
-                           1, (0, 0, 0))
+        self.render_board()
+        self.render_pieces()
